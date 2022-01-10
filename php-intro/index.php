7ce4b1e1187f12e9c69f0d3e618957e240d3d182
Christian Oliveira Leichtweis <chris99leichtweiss@gmail.com>
2022-01-10 16:08:38 -0300

    função valida idade com switch case

--- php-intro/index.php
+++ php-intro/index.php
@@ -7,6 +7,27 @@ $categorias[] = 'adolescente';
 $categorias[] = 'adulto';
 $categorias[] = 'idoso';
 
-var_dump($categorias);
+// print_r($categorias); // Printa os valores de categorias.
+
+user('Eduardo', 89, $categorias);
+
+function user($name, $age, $array) {
+    switch ($age) {
+        case ($age <= 10):
+            echo "É $array[0].";
+            break;
+        case ($age >= 11 && $age <= 18):
+            echo "É $array[1].";
+            break;
+        case ($age >= 19 && $age <= 65):
+            echo "É $array[2].";
+            break;
+        case ($age >= 66):
+            echo "É $array[3].";
+            break;
+    }
+}
+
+// var_dump($nome, $idade); // Printa nome e idade.
 
 ?>
